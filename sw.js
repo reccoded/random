76dcb7e5abd2a9cbd4ca1a8aeaf116b19edf2909
reccoded <recmeins@gmail.com>
2026-09-21 00:39:52 +0900

    Add files via upload

--- sw.js
+++ sw.js
@@ -1,4 +1,4 @@
-const CACHE="random-pwa-v11";
+const CACHE="random-pwa-v12";
 const ASSETS=["./","./index.html","./manifest.webmanifest","./icon-192.png","./icon-512.png","./icon-maskable-512.png"];
 self.addEventListener("install",event=>{
   event.waitUntil(caches.open(CACHE).then(cache=>cache.addAll(ASSETS)).then(()=>self.skipWaiting()));
