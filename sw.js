f949f99826a06c948f9cb1325b8cc4f9cc1f3bdd
reccoded <recmeins@gmail.com>
2026-09-21 01:00:37 +0900

    Add files via upload

--- sw.js
+++ sw.js
@@ -1,24 +1,49 @@
-const CACHE="random-pwa-v13";
-const ASSETS=["./","./index.html","./manifest.webmanifest","./icon-192.png","./icon-512.png","./icon-maskable-512.png"];
-self.addEventListener("install",event=>{
-  event.waitUntil(caches.open(CACHE).then(cache=>cache.addAll(ASSETS)).then(()=>self.skipWaiting()));
+const CACHE='random-pwa-v14';
+const ASSETS=[
+  '/random/',
+  '/random/index.html',
+  '/random/manifest.webmanifest',
+  '/random/icon-192.png',
+  '/random/icon-512.png',
+  '/random/icon-maskable-512.png'
+];
+
+self.addEventListener('install', event => {
+  self.skipWaiting();
+  event.waitUntil(caches.open(CACHE).then(cache => cache.addAll(ASSETS)));
 });
-self.addEventListener("activate",event=>{
-  event.waitUntil(caches.keys().then(keys=>Promise.all(keys.filter(key=>key!==CACHE).map(key=>caches.delete(key)))).then(()=>self.clients.claim()));
+
+self.addEventListener('activate', event => {
+  event.waitUntil(
+    caches.keys()
+      .then(keys => Promise.all(keys.filter(k => k !== CACHE).map(k => caches.delete(k))))
+      .then(() => self.clients.claim())
+  );
 });
-self.addEventListener("fetch",event=>{
-  if(event.request.method!=="GET")return;
-  if(event.request.mode==="navigate"){
-    event.respondWith(fetch(event.request).then(response=>{
-      const copy=response.clone();
-      caches.open(CACHE).then(cache=>cache.put("./index.html",copy));
-      return response;
-    }).catch(()=>caches.match("./index.html")));
+
+self.addEventListener('fetch', event => {
+  if (event.request.method !== 'GET') return;
+  const url=new URL(event.request.url);
+  if (url.origin !== self.location.origin) return;
+
+  if (event.request.mode === 'navigate') {
+    event.respondWith(
+      fetch(event.request)
+        .then(response => {
+          const copy=response.clone();
+          caches.open(CACHE).then(cache => cache.put('/random/index.html',copy));
+          return response;
+        })
+        .catch(() => caches.match('/random/index.html'))
+    );
     return;
   }
-  event.respondWith(caches.match(event.request).then(cached=>cached||fetch(event.request).then(response=>{
-    const copy=response.clone();
-    caches.open(CACHE).then(cache=>cache.put(event.request,copy));
-    return response;
-  })));
+
+  event.respondWith(
+    caches.match(event.request).then(cached => cached || fetch(event.request).then(response => {
+      const copy=response.clone();
+      caches.open(CACHE).then(cache => cache.put(event.request,copy));
+      return response;
+    }))
+  );
 });
